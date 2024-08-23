ac4f4744572cbb2869a75fccbae5b7e5c220dd2d
brianleonel <112188352+brianleonel@users.noreply.github.com>
2024-08-23 16:53:01 -0300

    List.tsx

--- List.tsx
+++ List.tsx
@@ -1,7 +1,9 @@
-type Props={
+type Props={/*se define la estructura de Props, en este caso un array de string*/
     data: string[];
 }
-function List({data}:Props){
+function List({data}:Props){/*la funcion de :Props es q el componente list espera recibir una propiedad con la estructura de Props.
+En data se desestructura Props, se extrae las propiedades de Props, data tiene la estructura y valores de Props 
+*/
 return(
     <ul>
     {data.map((elemento)=>(<li>{elemento}</li>))}
